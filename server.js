11a0f12e369e07a3f694a84f6487d5daed64b87c
toshjonb <toshjonbe@gmail.com>
2026-04-07 11:48:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+
+// 🚨 ENG MUHIM QATOR
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
