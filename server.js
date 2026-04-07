8026709fd9b19aa4de802ce0f335aa8b04aa31e7
toshjonb <toshjonbe@gmail.com>
2026-04-07 11:52:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const path = require("path");
 
 const app = express();
 
-// 🚨 ENG MUHIM QATOR
 const PORT = process.env.PORT || 3000;
 
+// Static fayllar
 app.use(express.static(__dirname));
 
+// Test route (MUHIM)
+app.get("/test", (req, res) => {
+  res.send("WORKING ✅");
+});
+
+// Asosiy sahifa
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Server start
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
